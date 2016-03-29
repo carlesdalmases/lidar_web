a851e57b06a11f9f82685e2d320c6364f93343af
Carles Dalmases <c.dalmases@gmail.com>
2016-03-29 14:45:17 +0200

    Canvas Point
    New approach to canvas point layer

--- src/main_.js
+++ src/main_.js
@@ -61,7 +61,6 @@ function main_()
 
 	var geojsonLayer = L.Proj.geoJson(geojson, {pointToLayer: function(type, latlng){return L.marker(latlng, {icon: myIcon});}});
 
-
 	// ***************************************************************						
 
 	//Afegeixo les layers al map
@@ -89,16 +88,15 @@ function main_()
 	//Control de capes
 	L.control.layers(baseMaps, overlayMaps).addTo(map);
 	
-
-
-	var URL_query = 'http://dalmases.ddns.net:8983/solr/lidar/select?q=*:*&wt=json&json.wrf=?&fl=xy,z,class&rows=10000';
+	var URL_query = 'http://dalmases.ddns.net:8983/solr/lidar/select?q=*:*&wt=json&json.wrf=?&fl=xy,z,class&sort=random_1234%20desc&rows=10000';
 	
 	query_server(URL_query).then
 	(
 			function(df)
 			{
 				wkt = new Wkt.Wkt();
-								
+				
+				console.time("concatenation");				
 				$.each(df.response.docs, function(index, value)
 				{
 					wkt.read(value.xy);
@@ -116,10 +114,47 @@ function main_()
 				});
 				
 				console.log("GeoJson preparat!");
+				console.timeEnd("concatenation");
 				
+				console.time("concatenation");
 				//Afegeixo tot el Json a la layer
 				geojsonLayer.addData(geojson);
+				console.timeEnd("concatenation");
+				
+				//CANVAS
+				/*
+				
+				http://bl.ocks.org/sumbera/11114288
+				
+				L.canvasOverlay()
+			            .drawing(drawingOnCanvas)
+			            .addTo(leafletMap);
+			    */
 			}
 	);
+	
+
+	// function drawingOnCanvas
+	/*
+	function drawingOnCanvas(canvasOverlay, params) 
+	{
+	    var ctx = params.canvas.getContext('2d');
+	    ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
+	    ctx.fillStyle = "rgba(255,116,0, 0.2)";
+	
+	    for (var i = 0; i < data.length; i++) 
+	    {
+	        var d = data[i];
+	        if (params.bounds.contains([d[0], d[1]])) 
+	        {
+	            dot = canvasOverlay._map.latLngToContainerPoint([d[0], d[1]]);
+	            ctx.beginPath();
+	            ctx.arc(dot.x, dot.y, 3, 0, Math.PI * 2);
+	            ctx.fill();
+	            ctx.closePath();
+	        }
+	    }
+	};
+	*/
 }
 
