6cdf8f326b066c1bd3d42290771aa3a5933cffe8
Carles Dalmases <c.dalmases@gmail.com>
2016-03-30 12:11:57 +0200

    Canvas Layer
    Creació d'una capa CANVAS amb els punts.

--- src/main_.js
+++ src/main_.js
@@ -1,9 +1,5 @@
 //main_()
 
-//Objecte amb tota la configuració per defecte
-//var servercatalog = new ServerCatalog();
-
-
 function main_()
 {
 
@@ -21,51 +17,35 @@ function main_()
 	    format: 'image/jpeg',
 	    crs: crs25831,
 	    continuousWorld: true,
+	    maxZoom: crs25831.options.resolutions.length,
+	    minZoom: 0,
 	    attribution: 'Institut Cartogr&agrave;fic i Geol&oacute;gic de Catalunya -ICGC',
 	});
 
 	var serveiOrtoCache = L.tileLayer.wms("http://mapcache.icc.cat/map/bases/service?", {
-	       layers: 'orto',
-	       format: 'image/jpeg',
-	       crs: crs25831,
-	       continuousWorld: true,
-	       attribution: 'Institut Cartogr&agrave;fic i Geol&oacute;gic de Catalunya -ICGC',
+		layers: 'orto',
+		format: 'image/jpeg',
+		crs: crs25831,
+		continuousWorld: true,
+		maxZoom: crs25831.options.resolutions.length,
+		minZoom: 0,	
+		attribution: 'Institut Cartogr&agrave;fic i Geol&oacute;gic de Catalunya -ICGC',
 	});
 
 	var serveitopoGrisCache = L.tileLayer.wms("http://mapcache.icc.cat/map/bases/service?", {
-	      layers: 'topogris',
-	      format: 'image/jpeg',
-	      crs: crs25831,
-	      continuousWorld: true,
-	      attribution: 'Institut Cartogr&agrave;fic i Geol&oacute;gic de Catalunya -ICGC',
+		layers: 'topogris',
+		format: 'image/jpeg',
+		crs: crs25831,
+		continuousWorld: true,
+		maxZoom: crs25831.options.resolutions.length,
+		minZoom: 0,	
+		attribution: 'Institut Cartogr&agrave;fic i Geol&oacute;gic de Catalunya -ICGC',
 	});
 	
-	// ***************************************************************						
-	// Objecte GeoJson amb les coordenades dels punts
-	
-	var geojson = {
-					"type": "FeatureCollection",
-    				"features": []
-    			  };
-
-	// ***************************************************************						
-
-	// ***************************************************************
-	//Creo la layer de punts i l'afegeixo al mapa
-
-
-	var myIcon = L.divIcon({className: 'leaflet-div-icon3'});
-	//var myIcon = L.divIcon({className: 'leaflet-div-icon'});
-
-
-
-	var geojsonLayer = L.Proj.geoJson(geojson, {pointToLayer: function(type, latlng){return L.marker(latlng, {icon: myIcon});}});
-
-	// ***************************************************************						
 
 	//Afegeixo les layers al map
 	var map = L.map('map', {
-	      layers: [serveitopoGrisCache, geojsonLayer],
+	      layers: [serveitopoGrisCache],
 	      crs: crs25831,
 	      continuousWorld: true,
 	      worldCopyJump: false,
@@ -73,88 +53,72 @@ function main_()
 	      zoom: 7,
 	});
 
-	var baseMaps = {
-	             "Topogr&agrave;fic": serveiTopoCache,
-	             "Topogr&agrave;fic gris": serveitopoGrisCache,
-	             "Ortofoto": serveiOrtoCache
+	//Llegenda
+	var baseMaps = 
+	{
+		"Topogr&agrave;fic": serveiTopoCache,
+		"Topogr&agrave;fic gris": serveitopoGrisCache,
+		"Ortofoto": serveiOrtoCache
 	};
-
 	
-	var overlayMaps = {"Punts LIDAR":geojsonLayer};
+	//var overlayMaps = {"Punts LIDAR":geojsonLayer};
 	
 	//Escala
 	L.control.scale({imperial: false}).addTo(map);
 	
 	//Control de capes
-	L.control.layers(baseMaps, overlayMaps).addTo(map);
-	
-	var URL_query = 'http://dalmases.ddns.net:8983/solr/lidar/select?q=*:*&wt=json&json.wrf=?&fl=xy,z,class&sort=random_1234%20desc&rows=10000';
+	//L.control.layers(baseMaps, overlayMaps).addTo(map);
+	L.control.layers(baseMaps).addTo(map);
+
+	//Consulta al SOLR
+	var URL_query = 'http://dalmases.ddns.net:8983/solr/lidar/select?q=*:*&wt=json&json.wrf=?&fl=xy,z,class&sort=random_1234%20desc&rows=50000';
 	
 	query_server(URL_query).then
 	(
 			function(df)
 			{
-				wkt = new Wkt.Wkt();
-				
-				console.time("concatenation");				
-				$.each(df.response.docs, function(index, value)
-				{
-					wkt.read(value.xy);
-					geojson.features.push(
-											{
-				  								"type": "Feature",
-												"properties": {"z": value.z, "class": value.class},
-												"geometry": {
-																"type": "Point", 
-									       						"coordinates": [wkt.components[0].x, wkt.components[0].y]
-									       					},
-				  								"crs": {"type": "name","properties": {"name": "EPSG:25831"}}
-	  			  							}
-										 );
-				});
-				
-				console.log("GeoJson preparat!");
-				console.timeEnd("concatenation");
-				
-				console.time("concatenation");
-				//Afegeixo tot el Json a la layer
-				geojsonLayer.addData(geojson);
-				console.timeEnd("concatenation");
-				
 				//CANVAS
-				/*
-				
-				http://bl.ocks.org/sumbera/11114288
-				
-				L.canvasOverlay()
+				/* http://bl.ocks.org/sumbera/11114288 */
+				var puntsLIDAR = L.canvasOverlay()
+						.params({data: df})
 			            .drawing(drawingOnCanvas)
-			            .addTo(leafletMap);
-			    */
+			            .addTo(map);
+			    
+				//Afegeixo la capa a la llegenda
+				//TODO
 			}
 	);
 	
 
-	// function drawingOnCanvas
-	/*
+	// **************************
+	// drawingOnCanvas()
+	// **************************
 	function drawingOnCanvas(canvasOverlay, params) 
 	{
+		wkt = new Wkt.Wkt();
 	    var ctx = params.canvas.getContext('2d');
 	    ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
-	    ctx.fillStyle = "rgba(255,116,0, 0.2)";
-	
-	    for (var i = 0; i < data.length; i++) 
-	    {
-	        var d = data[i];
-	        if (params.bounds.contains([d[0], d[1]])) 
-	        {
-	            dot = canvasOverlay._map.latLngToContainerPoint([d[0], d[1]]);
-	            ctx.beginPath();
-	            ctx.arc(dot.x, dot.y, 3, 0, Math.PI * 2);
-	            ctx.fill();
-	            ctx.closePath();
-	        }
-	    }
-	};
-	*/
-}
+	    ctx.fillStyle = "rgba(255,0,0, 1)";
+	    
+	    //Defineixo la projecció ETRS89-UTM31N segons el ICC
+	    proj4.defs('EPSG:25831','+proj=utm +zone=31 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
+		
+		$.each(params.options.data.response.docs, function(index, value)
+		{
+			wkt.read(value.xy);
+			
+			//Converteixo les coordenades a LAT/LONG
+			latlong = proj4('EPSG:25831','WGS84',[wkt.components[0].x, wkt.components[0].y]);
+			
+			//Creo el punt amb coordenades de pantalla
+			dot = canvasOverlay._map.latLngToContainerPoint([latlong[1], latlong[0]]);
+			ctx.beginPath();
+			//Dibuixo el punt
+			ctx.arc(dot.x, dot.y, 3, 0, Math.PI * 2);
+			//Omplo de color
+			ctx.fill();
+			ctx.closePath();			
+		});		
+	}; //Fi de drawingOnCanvas()
 
+} // Fi de main_()
